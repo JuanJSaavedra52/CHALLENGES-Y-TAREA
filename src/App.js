09a01a93c7c255fa7a8c3ca9869d5e9704b3eefd
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-24 20:31:21 +0000

    Ajustes finales (CHALLENGE COMPLETADO)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import './App.css';
+import './FirstApp'
+import FirstApp from './FirstApp';
 
 const App = () => {
 
   return (
     <>
-      <h1> Hola Mundo! </h1>
-      <h2> Bienvenido </h2>
+      <FirstApp />
     </>
   )
 }
