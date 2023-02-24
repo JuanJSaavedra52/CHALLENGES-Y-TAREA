37d9be5d0ab7435753b0b55dca01cee25cb0ead3
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-24 20:13:30 +0000

    Eliminación y cambios necesarios al codigo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from "react"
-import { AddCategory } from './components/AddCategory';
-import { GifGrid } from './components/GifGrid';
+import './App.css';
 
-export const GifExpertApp = () => {
-
-  const [categories, setCategories] = useState(['Zodiac'])
-
-  const onAddCategory = ( category ) => {
-    setCategories(list => [...list, category])
-  }
+const App = () => {
 
   return (
     <>
-      <h1>GifExpert</h1>
-      <AddCategory onAddCategory={onAddCategory} />
-      {
-        categories.map(
-          (category, key) => 
-          {
-            return <GifGrid category={ category } key={key} />
-          }
-        )
-      }
+      <h1> Hola Mundo! </h1>
+      <h2> Bienvenido </h2>
     </>
   )
 }
+
+export default App;
--- src/hooks/useFetch.js
+++
@@ -1,4 +0,0 @@
-/*import { useEffect, useState } from "react";
-export const useFetch = ( url ) => {
-    const [state]
-}*/
