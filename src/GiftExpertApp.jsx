9298e11da54592eaa720af455e6175c95504e131
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-25 00:54:12 +0000

    CHALLENGE 4 COMPLETADO

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,27 +1,36 @@
+import React from "react";
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
 
 export const GiftExpertApp = () => {
 
-    const [categories, setCategories] = useState(['Zodiac'])
+    const [categories, setCategories] = useState(['first category'])
+    const [category, setCategory] = useState('')
 
-    const onAddCategory = (category) => {
+    const onAddCategory = () => {
         setCategories(list => [...list, category])
+        setCategory('')
+    }
+
+    const onSetCategory = (evt) => {
+        setCategory(evt.target.value)
     }
 
     return (
         <>
             <h1>GifExpert</h1>
-
-            <AddCategory onAddCategory={onAddCategory} />
-            {
-                categories.map(
-                    (category, key) => {
-                        return <GifGrid category={category} key={key} />
-                    }
-                )
-            }
+            <input type="text" value={category}
+                onChange={(event) => onSetCategory(event)} />
+            <button onClick={() => onAddCategory()}> Add Category </button>
+            <ol>
+                {
+                    categories.map(
+                        (category, key) => 
+                        {
+                            return <li key={key}> {category} </li>
+                        }
+                    )
+                }
+            </ol>
         </>
     )
 }
