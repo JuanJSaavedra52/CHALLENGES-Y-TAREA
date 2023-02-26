18943511610e9fa91baaecc2d785ae8dca068e4e
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-26 13:02:53 +0000

    CHALLENGE-5 COMPLETADO

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import FirstApp from './FirstApp';
 import { GiftExpertApp } from './GiftExpertApp';
@@ -11,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FirstApp value={0}/>
-    <GiftExpertApp />
+     <GiftExpertApp />
     <GifGrid />
   </React.StrictMode>
 );
