898ee3c9573cb442dac7cfcc695fd95586e8c4df
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-26 12:32:23 +0000

    PROBANDO

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import FirstApp from './FirstApp';
 import { GiftExpertApp } from './GiftExpertApp';
+import { GifGrid } from './components/GifGrid';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FirstApp value={0}/>
     <GiftExpertApp />
+    <GifGrid />
   </React.StrictMode>
 );
 
