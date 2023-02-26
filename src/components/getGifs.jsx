b04685a6890b6074b4c695fad1bc7548b4596dd6
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-26 13:52:30 +0000

    Funciona pero hay que eliminar el boton de no me toqué

--- src/components/getGifs.jsx
+++ src/components/getGifs.jsx
@@ -1,4 +1,4 @@
-export const GetGifs = async (category) => {
+export const getGifs = async (category) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=4KQW5PSHc6OT3DelUBSCEPVi8HAginfq&q=${category}&limit=25&offset=0&rating=g&lang=en`
     const resp = await fetch(url)
     const {data} = await resp.json()
